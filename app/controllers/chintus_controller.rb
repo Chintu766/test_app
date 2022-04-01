12c9dad530c18f5671f86303af9965385fc67ae3
chintusai <chintusai.maddi@prasthana.com>
2022-04-01 13:01:23 +0530

    crud

--- app/controllers/chintus_controller.rb
+++ app/controllers/chintus_controller.rb
@@ -1,5 +1,14 @@
 class ChintusController <  ApplicationController
-  def chintu
+  def show
+    @chintu = Chintu.new
+  end
+  def index
+     @chintus = Chintu.all
+  end
+  def new
 
+  end
+  def create
+    render plain:params[:chintu]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :articles
+  resources :chintus #, only:[:show,:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 #get 'hello', to:'application#hello'
   # Defines the root path route ("/")
