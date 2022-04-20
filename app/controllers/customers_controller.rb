5626b7d245c3bbbe9c7217ab36f508e622a51966
chintusai <chintusai.maddi@prasthana.com>
2022-04-20 18:15:45 +0530

    user_authentication

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,13 +1,16 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update]
   def new
     @customer = Customer.new
   end
   def edit
-    @customer = Customer.find(params[:id])
   end
-
+  def index
+     @customers = Customer.all
+  end
+  def show
+  end
   def update
-    @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to chintus_path
@@ -28,5 +31,8 @@ class CustomersController < ApplicationController
   private
   def customer_params
     params.require(:customer).permit(:username, :email, :password)
+  end
+  def set_customer
+  @customer = Customer.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
   get 'home',to: 'pages#home'
   get 'signup',to: 'customers#new'
   resources :customers,except:[:new]
+  get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
 end
